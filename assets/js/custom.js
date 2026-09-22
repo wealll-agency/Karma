0b3af5f72adae4b85bfbf420d70eb541462e8797
We Alll <weallldevelopment@gmail.com>
2026-09-22 20:18:56 +0530

    UI enhancements: Fix BOM encodings, update educator forms, add premium partner marquee, fix mobile gallery & about overlays

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * KARMA INTERNATIONAL - Custom JavaScript
  * assets/js/custom.js
  */
@@ -127,5 +127,128 @@
         updateSlider();
       });
     }
+    // 6. Stats Counter Animation
+    $('.counter').each(function () {
+      $(this).prop('Counter', 0).animate({
+        Counter: $(this).data('target')
+      }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        }
+      });
+    });
+
+    // 7. Events Carousel (Owl Carousel fallback/alternative)
+    if ($('.events-carousel').length) {
+      var eventsOwl = $('.events-carousel').owlCarousel({
+        loop: true,
+        margin: 24,
+        nav: false,
+        dots: false,
+        autoplay: false,
+        responsive: {
+          0: { items: 1 },
+          768: { items: 2 },
+          1024: { items: 2 },
+          1200: { items: 3 }
+        }
+      });
+      $('#upcomingNextBtn').click(function () { eventsOwl.trigger('next.owl.carousel'); });
+      $('#upcomingPrevBtn').click(function () { eventsOwl.trigger('prev.owl.carousel'); });
+    }
+
+    // 8. Gallery Carousel
+    if ($('.gallery-carousel').length) {
+      var galleryOwl = $('.gallery-carousel').owlCarousel({
+        loop: true,
+        margin: 15,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive: {
+          0: { items: 1 },
+          576: { items: 2 },
+          768: { items: 3 },
+          1024: { items: 4 }
+        }
+      });
+      $('#galleryNextBtn').click(function () { galleryOwl.trigger('next.owl.carousel'); });
+      $('#galleryPrevBtn').click(function () { galleryOwl.trigger('prev.owl.carousel'); });
+    }
+
+    // 9. Premium Testimonial Carousel
+    if ($('.premium-testimonial-carousel').length) {
+      var sponsorOwl = $('.premium-testimonial-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000
+      });
+      $('.premium-next-btn').click(function () { sponsorOwl.trigger('next.owl.carousel'); });
+      $('.premium-prev-btn').click(function () { sponsorOwl.trigger('prev.owl.carousel'); });
+    }
+
+    // 10. Gallery Filter & Search Logic
+    $('.gallery-filter-btn').on('click', function() {
+        $('.gallery-filter-btn').removeClass('active');
+        $(this).addClass('active');
+        let filter = $(this).text().toLowerCase();
+        
+        if (filter === 'all') {
+            $('.grid-item').fadeIn(400);
+        } else {
+            $('.grid-item').hide();
+            $('.grid-item').filter(function() {
+                return $(this).data('category') === filter;
+            }).fadeIn(400);
+        }
+    });
+
+    // 11. Gallery Search
+    $('.gallery-search-input').on('keyup', function() {
+        let query = $(this).val().toLowerCase();
+        $('.grid-item').each(function() {
+            let title = $(this).data('title') ? $(this).data('title').toLowerCase() : '';
+            if (title.indexOf(query) > -1) {
+                $(this).fadeIn(400);
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+
+    // 12. Image Lightbox Logic
+    $('.grid-item').not('.grid-text-item').on('click', function() {
+        let imgSrc = $(this).find('img').attr('src');
+        if (imgSrc) {
+            $('#lightboxImage').attr('src', imgSrc);
+            $('#imageModal').modal('show');
+        }
+    });
+
+    // 13. Video Modal Logic
+    $('.open-video-modal').on('click', function() {
+        let url = $(this).data('video-url');
+        if (url) {
+            let encodedUrl = encodeURIComponent(url);
+            let iframe = `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=${encodedUrl}&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>`;
+            $('#videoContainer').html(iframe);
+            $('#videoModal').modal('show');
+        }
+    });
+
+    // Clear video src when modal closes to stop playback
+    $('#videoModal').on('hidden.bs.modal', function () {
+        $('#videoContainer').html('');
+    });
   });
 })(jQuery);
+
+
+
